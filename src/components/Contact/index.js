fd7fc0fe466983be2c88e72b25f9a5bf9660ca0f
Valerie <valerie.tram.anh.n@gmail.com>
2022-01-05 21:35:34 -0500

    control state with a controlled commponent

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,26 +1,42 @@
+import React, { useState } from 'react';
+
+const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+const { name, email, message } = formState;
+
+function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+}
+
 function ContactForm() {
 
     // JSX
     return (
         <section>
           <h1>Contact me</h1>
-          <form id="contact-form">
+          <form id="contact-form" onSubmit={handleSubmit}>
             <div>
               <label htmlFor="name">Name:</label>
-              <input type="text" name="name" />
+              <input type="text" defaultValue={name} name="name" />
             </div>
             <div>
               <label htmlFor="email">Email address:</label>
-              <input type="email" name="email" />
+              <input type="email" defaultValue={email} name="email" onChange={handleChange} />
             </div>
             <div>
               <label htmlFor="message">Message:</label>
-              <textarea name="message" rows="5" />
+              <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
             </div>
             <button type="submit">Submit</button>
           </form>
         </section>
         );
-    }
+}
+
+function handleChange(e) {
+    setFormState({...formState, name: e.target.value })
+  }
+  
+console.log(formState);
     
 export default ContactForm;
